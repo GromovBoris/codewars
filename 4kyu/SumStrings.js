72dd1ad72a8dcbc675c1ba4bae8f0d30ce98b27d
GromovBoris <drunkgonzo@gmail.com>
2023-07-31 23:26:22 +0300

    sumStrings done

---
+++ 4kyu/SumStrings.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Given the string representations of two integers, return the string representation of the sum of those integers.
+
+function sumStrings(a, b) {
+  console.log(String(BigInt(a)) + BigInt(b));
+}
+
+sumStrings("712569312664357328695151392", "8100824045303269669937");
